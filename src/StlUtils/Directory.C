824efba62804301d3aff904c5f79b2f86925c147
houghton <>
1995-11-05 13:48:12 +0000

    New implementation

--- src/StlUtils/Directory.C
+++ src/StlUtils/Directory.C
@@ -10,18 +10,20 @@
 // Revision History:
 //
 // $Log$
-// Revision 1.2  1995/11/05 13:22:38  houghton
-// New Implementation
+// Revision 1.3  1995/11/05 13:48:11  houghton
+// New implementation
 //
 //
 
 #include "Directory.hh"
+#include "User.hh"
+#include "UserGroup.hh"
 #include "Compare.hh"
-
 #include <unistd.h>
 #include <dirent.h>
+#include <cstring>
 
-#ifdef  CLUE_DEBUG
+#if defined( CLUE_DEBUG )
 #define inline
 #include <Directory.ii>
 #endif
@@ -80,38 +82,142 @@ DirSortTime::operator () ( const FileStat & one, const FileStat & two ) const
   return( ::compare( one.getModificationTime(), two.getModificationTime() ) );
 }
 
+class DirSortUser : public SortCompareBase< FileStat >
+{
+public:
+  int operator () ( const FileStat & one, const FileStat & two ) const;
+};
+
+int
+DirSortUser::operator () ( const FileStat & one, const FileStat & two ) const
+{
+  User uOne( one.getUID() );
+  User uTwo( two.getUID() );
+  
+  return( uOne.compare( uTwo ) );
+}
+
+class DirSortUserGroup : public SortCompareBase< FileStat >
+{
+public:
+  int operator () ( const FileStat & one, const FileStat & two ) const;
+};
+
+int
+DirSortUserGroup::operator () ( const FileStat & one, const FileStat & two ) const
+{
+  UserGroup gOne( one.getGID() );
+  UserGroup gTwo( two.getGID() );
+  
+  return( one.compare( two ) );
+}
+
 
 const Directory::DirOrder SortName( DirSortName() );
 const Directory::DirOrder SortExt( DirSortExt() );
 const Directory::DirOrder SortSize( DirSortSize() );
 const Directory::DirOrder SortTime( DirSortTime() );
+const Directory::DirOrder SortUser( DirSortUser() );
+const Directory::DirOrder SortUserGroup( DirSortUserGroup() );
 
+Directory::~DirField( void )
+{
+}
+
+Directory::
+  
+  
 Directory::~Directory( void )
 {
 }
 
 bool
-Directory::set( const char * path, bool recurs = false )
+Directory::set( const char * path, Options options )
 {
-  Str dirName( path );
+  Str	    dirName( path );
+  FilePath  dirPath;
+  FilePath  matchPath;
   
   size_t wild = dirName.find_first_of( "*?" );
 
-  size_t pathEnd = dirName.rfind( DIR_DELIM, wild );
+  size_t pathEnd = dirName.rfind( FilePath::DirDelim, wild );
 
   if( pathEnd == Str::npos )
-    dirName = ".";
+    {
+      matchPath = dirName;
+      dirName = ".";
+    }
   else
-    dirName.remove( pathEnd );
+    {
+      matchPath = dirName.after( pathEnd );
+      dirPath = dirName.before( pathEnd );
+      
+      dirName.remove( pathEnd );
+    }
 
   DIR * dir = opendir( dirName );
 
   if( ! dir )
     return( false );
+  else
+    return( readDir( dir, dirPath, matchPath, options ) );
+}
+
+bool
+Directory::readDir(
+  DIR *		    dir,
+  const FilePath &  dirPath,
+  const FilePath &  matchPath,
+  Options	    options
+  )
+{
+  Str match;
+  match = matchPath.getFullName().at( (size_t)0, matchPath.getFullName().find( FilePath::DirDelim ) );
+
+  for( struct dirent * dEnt = readdir( dir );
+       dEnt != 0;
+       dEnt = readdir( dir ) )
+    {
+      if( ! all && dEnt->d_name[0] == "." )
+	continue;
+      
+      FilePath name( dEnt->d_name );
+      
+      if( match.size() && ! name.match( match ) )
+	continue;
+
+      name.setPrefix( dirPath );
+      
+      FileStat fs( name );
+
+      list.push_back( fs );
+
+      
+      if( fs.good() && fs.isDir() &&
+	  strcmp( dEnt->d_name, "." ) && strcmp( dEnt->d_name, ".." ) &&
+	  (recurs || match.size() < matchPath.getFullName().size() ) )
+	{
+	  FilePath subDirPath;
+	  FilePath subMatchPath;
+
+	  subDirPath = name;
+	  subMatchPath = matchPath.getFullName().after( match.size() );
+
+	  DIR * subDir = opendir( subDirPath );
+
+	  if( ! subDir ||
+	      ! readDir( subDir, subDirPath, subMatchPath, recurs ) )
+	    // FIXME - strange error;
+	    break;
+	}
+    }
 
   return( true );
 }
+	    
+      
 
+  
 bool
 Directory::set(
   const char *			path,
--- src/StlUtils/Directory.hh
+++ src/StlUtils/Directory.hh
@@ -14,25 +14,38 @@
 // Revision History:
 //
 // $Log$
-// Revision 1.2  1995/11/05 13:22:39  houghton
-// New Implementation
+// Revision 1.3  1995/11/05 13:48:11  houghton
+// New implementation
 //
 //
 
-#include <ClueConfig.hh>
-
+#ifdef CLUE_SHORT_FN
+#include <ClueCfg.hh>
+#include <SortOrdr.hh>
 #include <FileStat.hh>
-#include <SortOrder.hh>
-
+#include <BinMask.hh>
 #include <vector>
 #include <set>
-
 #include <climits>
+#include <dirent.h>
+#else
+#include <ClueConfig.hh>
+#include <SortOrder.hh>
+#include <FileStat.hh>
+#include <BinMask.hh>
+#include <vector>
+#include <set>
+#include <climits>
+#include <dirent.h>
+#endif
 
-#ifdef  CLUE_DEBUG
+
+#if defined( CLUE_DEBUG )
 #define inline
 #endif
 
+
+  
 class Directory
 {
 
@@ -41,46 +54,219 @@ public:
   typedef vector< FileStat >	List;
   typedef List::iterator	iterator;
   typedef SortOrder< FileStat >	DirOrder;
-  
-  static const DirOrder   SortName;
-  static const DirOrder   SortExt;
-  static const DirOrder   SortSize;
-  static const DirOrder   SortTime;
-  
-  inline Directory( const char * path, bool recurs = false );
-  inline Directory( const char * path,
-		    const DirOrder & order,
-		    bool recurs = false );
+  typedef BitMask		Options;
 
+  class DirField
+  {
+  public:
+
+    inline DirField( void ):
+    virtual ~DirField( void );
+    
+    enum CompareType
+    {
+      Equal,
+      NotEqual,
+      Less,
+      More,
+      LessEqual,
+      MoreEqual
+    };
+
+    inline void	    addRef( void );
+    inline bool	    delRef( void );
+    
+    virtual bool	match( const FileStat & stat ) const == 0;
+    virtual DirField *	newCopy( void ) const = 0;
+    virtual ostream &	dumpInfo( ostream & dest = cerr );
+    
+  protected:
+    
+    CompareType  comp;
+    size_t	 refCount;
+    
+  };
+
+  class DirFieldFileName : public DirField
+  {
+  public:
+
+    inline DirFieldFileName( const DirFieldName & from );
+    virtual DirFieldFileName( void ) {};
+    
+    virtual bool match( const FileStat & stat ) const;
+    virtual DirField *	newCopy( void ) const;
+    
+    inline DirFieldFileName	operator == ( const char * rhs ) const;
+    inline DirFieldFileName	operator != ( const char * rhs ) const;
+    inline DirFieldFileName	operator <  ( const char * rhs ) const;
+    inline DirFieldFileName	operator >  ( const char * rhs ) const;
+    inline DirFieldFileName	operator <= ( const char * rhs ) const;
+    inline DirFieldFileName	operator >= ( const char * rhs ) const;
+    
+    virtual ostream &	dumpInfo( ostream & dest = cerr );
+    
+  private:
+    
+    Str value;
+  };
+
+  class DirFieldSize : public DirField
+  {
+  public:
+
+    inline DirFieldSize( const DirFieldSize & from );
+    virtual DirFieldSize( void ) {};
+    
+    virtual bool match( const FileStat & stat ) const;
+    virtual DirField *	newCopy( void ) const;
+    
+    inline DirFieldSize	operator == ( time_t rhs ) const;
+    inline DirFieldSize	operator != ( time_t rhs ) const;
+    inline DirFieldSize	operator <  ( time_t rhs ) const;
+    inline DirFieldSize	operator >  ( time_t rhs ) const;
+    inline DirFieldSize	operator <= ( time_t rhs ) const;
+    inline DirFieldSize	operator >= ( time_t rhs ) const;
+
+    virtual ostream &	dumpInfo( ostream & dest = cerr );
+    
+  private:
+
+    size_t value;
+  };
+  
+  class Where
+  {
+  public:
+
+    inline Where( const DirField & fld );
+    inline Where( const Where & where );
+    inline Where( const Where & left, bool andLR, const Where & right );
+
+    inline Where( void );
+
+    inline Where	    operator && ( const Where & where ) const;
+    inline Where	    operator || ( const Where & where ) const;
+
+    friend inline Where	    operator && ( const DirField & fld,
+					  const Where &	    where );
+    friend inline Where	    operator || ( const DirField & fld,
+					  const Where &	    where );
+
+    inline void		addRef( void );
+    inline bool		delRef( void );
+
+    ostream &		dumpInfo( ostream &	dest = err,
+				  const char *	prefix = "    ",
+				  bool		showVer = true ) const;
+
+    const char *	getVersion( void ) const;
+    static const char version[];
+
+  protected:
+
+    friend class Directory;
+    
+    inline bool			isField( void ) const;
+    inline bool			isAnd( void ) const;
+    inline const DataFld &	getField( void ) const;
+    inline const DataWhere &	getFirst( void ) const;
+    inline const DataWhere &	getSecond( void ) const;
+  
+  private:
+  
+    DirField *	fld;
+
+    here *	left;
+    bool	and;
+    Where *	right;
+
+    size_t    refCount;
+    size_t    leftDepth;
+    size_t    rightDepth;
+    
+  };
+
+  
+  static const DirOrder	    SortName;
+  static const DirOrder	    SortExt;
+  static const DirOrder	    SortSize;
+  static const DirOrder	    SortTime;
+  static const DirOrder	    SortUser;
+  static const DirOrder	    SortUserGroup;
+
+  static const Options	    Default;
+  static const Options	    Recurs;
+  static const Options	    All;
+
+  
+  
+  inline Directory( const char * path, const Options & opt = Default );
+  inline Directory( const char *	path,
+		    const DirOrder &	order,
+		    const Options &	opt = Default );
+
+  inline Directory( const Where & where, const Options & opt = Default );
+  inline Directory( const Where &	where,
+		    const DirOrder &	order,
+		    const Options &	opt = Default );
+  
   inline Directory( const Directory & from );
   
   virtual ~Directory( void );
 
   inline iterator	begin( void );
   inline iterator	end( void );
+  inline size_t		size( void ) const;
 
-  bool		set( const char * path, bool recurs = false );
+  bool		set( const char * path, const Options & opt = Default );
   bool		set( const char *	path,
   		     const DirOrder &   order,
-  		     bool recurs =	false );
+  		     const Options &	opt = Default );
+
+  bool		set( const Where &	where,
+		     const Options &	opt = Default );
+  
+  bool		set( const Where &	where,
+  		     const DirOrder &   order,
+		     const Options &	opt = Default );
   
   bool		sort( const DirOrder & order );
+
+  bool		exclude( const char * name );
+  bool		excludeDir( const char * path );
+  bool		excludeUser( const char * user );
+  bool		excludeGroup( const char * group );
+  bool		excludeMode( mode_t mode );
+  
+  bool		resetExclude( void );
   
   virtual bool	    	good( void ) const;
   virtual const char * 	error( void ) const;
   virtual const char *	getClassName( void ) const;
+  virtual ostream &	toStream( ostream & dest = cout ) const;
+  virtual ostream &	dumpInfo( ostream &	dest = cerr,
+				  const char *  prefix = "    ",
+				  bool		showVer = true ) const;
 
+  virtual const char *	getVersion( void ) const;
+    
   static const char version[];
   
 protected:
 
 private:
 
+  bool	readDir( DIR *		    dir,
+		 const FilePath &   dirPath,
+		 const FilePath &   matchPath,
+		 Options	    options );
+  
   List	list;
   
 };
 
-#ifndef inline
+#if !defined( inline )
 #include <Directory.ii>
 #else
 #undef inline
