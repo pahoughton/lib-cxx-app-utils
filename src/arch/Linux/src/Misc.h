29a8a7450a7e8b64dbd80953ed404ed077898561
houghton <>
1995-11-05 12:17:57 +0000

    Initial Misc.h

---
+++ src/arch/Linux/src/Misc.h
@@ -0,0 +1,40 @@
+#ifndef _Misc_h_
+#define _Misc_h_
+/*********************************************************************
+ *
+ * Title:            Misc.h
+ *
+ * Description:
+ *
+ *
+ *
+ * Notes:
+ *
+ * Programmer:	    Paul Houghton - (houghton@cworld.wiltel.com)
+ *
+ * Start Date:	    05/10/95 16:02
+ *
+ * Modification History:
+ *
+ * $Log$
+ * Revision 1.1  1995/11/05 12:17:57  houghton
+ * Initial Misc.h
+ *
+ *
+ *********************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* ! def _Misc_h_ */
