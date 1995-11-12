e928390a798bed63a903fb6d0a6364b3b5526c7e
houghton <>
1995-11-12 18:05:09 +0000

    Change LogLevel::XXXX to LogLevel::Xxxxx. Change setName to return true if changed, false if not.

--- src/StlUtils/LogLevel.hh
+++ src/StlUtils/LogLevel.hh
@@ -14,7 +14,11 @@
 //
 // 
 // $Log$
-// Revision 2.3  1995/11/10 14:11:43  houghton
+// Revision 2.4  1995/11/12 18:05:09  houghton
+// Change LogLevel::XXXX to LogLevel::Xxxxx.
+// Change setName to return true if changed, false if not.
+//
+// Revision 2.3  1995/11/10  14:11:43  houghton
 // Cleanup (move final endif)
 //
 // Revision 2.2  1995/11/10  14:08:37  houghton
@@ -51,22 +55,28 @@ public:
 
   typedef Bitmask Level;
 
-  static const Level	NONE;
-  static const Level	ERROR;
-  static const Level	ERR;
-  static const Level	WARNING;
-  static const Level	WARN;
-  static const Level	USER_1;
-  static const Level	USER_2;
-  static const Level	USER_3;
-  static const Level	USER_4;
-  static const Level	INFO;
-  static const Level	TEST;
-  static const Level	DEBUG;
-  static const Level	FUNCT;
-  static const Level	ALL;
+  static const Level	None;
+  static const Level	Error;
+  static const Level	Err;
+  static const Level	Warning;
+  static const Level	Warn;
+  static const Level	App1;
+  static const Level	App2;
+  static const Level	App3;
+  static const Level	App4;
+  static const Level	App5;
+  static const Level	App6;
+  static const Level	Lib1;
+  static const Level	Lib2;
+  static const Level	Lib3;
+  static const Level	Lib4;
+  static const Level	Info;
+  static const Level	Test;
+  static const Level	Debug;
+  static const Level	Funct;
+  static const Level	All;
   
-  inline LogLevel( const Level out = NONE );
+  inline LogLevel( const Level out = None );
   inline LogLevel( const char * out );
   
   inline Level	    setOutput( const Level out );
@@ -81,7 +91,7 @@ public:
   const char *	    getName( const Level level ) const;
   const char *	    getLevelNames( const Level level ) const;
   
-  inline void	    setName( const Level level, const char * name );
+  bool		    setName( const Level level, const char * name );
 
   inline bool 	    shouldOutput( void ) const;
   inline bool	    willOutput( const Level outLevel ) const;
