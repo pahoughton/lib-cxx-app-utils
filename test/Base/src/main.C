6a080ba0bbafa2e9f439f2a789f87f1be774cc33
houghton <>
1996-05-01 11:05:28 +0000

    Run all test.

--- test/Base/src/main.C
+++ test/Base/src/main.C
@@ -10,6 +10,9 @@
 // Revision History:
 //
 // $Log$
+// Revision 2.3  1996/05/01 11:05:28  houghton
+// Run all test.
+//
 // Revision 2.2  1996/02/29 19:11:09  houghton
 // *** empty log message ***
 //
@@ -24,6 +27,7 @@
 #if !defined( CLUE_SHORT_FN )
 #include <TestConfig.hh>
 #include <LibTest.hh>
+#include "TestFunctProto.hh"
 #include <iostream>
 #else
 #include <TestCfg.hh>
@@ -31,145 +35,11 @@
 #include <iostream>
 #endif
 
-bool	tArraySize( LibTest & tester );
-bool	tAbsLong( LibTest & tester );
-bool	tBit( LibTest & tester );
-bool	tBitmask( LibTest & tester );
-bool	tLog( LibTest & tester );
-bool	tParam( LibTest & tester );
 
-bool	tStrSet( LibTest & tester );
-
-#if defined( ALL )
-// Utils.hh
-bool	tArraySize( LibTest & tester );
-bool	tDwordAlign( LibTest & tester );
-bool	tMemOverlap( LibTest & tester );
-bool	tAbsLong( LibTest & tester );
-bool	tIsBaseDigit( LibTest & tester );
-bool	tUnionOf( LibTest & tester );
-bool	tRound( LibTest & tester );
-// Bit.hh
-bool	tBit( LibTest & tester );
-// Compare.hh
-bool	tCompare( LibTest & tester );
-// StringUtils.hh
-bool	tSafeStrcpy( LibTest & tester );
-bool	tCharToInt( LibTest & tester );
-bool	tStripWhite( LibTest & tester );
-bool	tStringSearch( LibTest & tester );
-bool	tStringCaseCompare( LibTest & tester );
-bool	tStringLower( LibTest & tester );
-bool	tStringUpper( LibTest & tester );
-bool	tStringCapitalize( LibTest & tester );
-bool	tStringTo( LibTest & tester );
-bool	tbasename( LibTest & tester );
-// BinStream.hh
-// RandomBinStream.hh
-// HeapBinStream.hh
-bool	tHeapBinStream( LibTest & tester );
-// Str.hh
-bool	tStr( LibTest & tester );
-// SubStr.hh
-bool	tSubStr( LibTest & tester );
-// RegexScan.hh
-bool	tRegexScan( LibTest & tester );
-// DateTimeUtils.hh
-bool	tDateTimeUtils( LibTest & tester );
-// DateTime.hh
-bool	tDateTime( LibTest & tester );
-// DateRange.hh
-bool	tDateRange( LibTest & tester );
-// DateRangeDaily.hh
-bool	tDateRangeDaily( LibTest & tester );
-// DateRangeWeekly.hh
-bool	tDateRangeWeekly( LibTest & tester );
-// Bitmask.hh
-bool	tBitmask( LibTest & tester );
-// SortOrder.hh
-bool	tSortOrder( LibTest & tester );
-// FilePath.hh
-bool	tFilePath( LibTest & tester );
-// User.hh
-bool	tUser( LibTest & tester );
-// UserGroup.hh
-bool	tUserGroup( LibTest & tester );
-// FileStat.hh
-bool	tFileStat( LibTest & tester );
-// LogLevel.hh LogBuf.hh Log.hh
-bool	tLog( LibTest & tester );
-// Param.hh
-bool	tParam( LibTest & tester );
-
-// libClue works with LibCommon
-bool	tWithCommon( LibTest & tester );
-// libClue works with Wcf
-bool	tWithWcf( LibTest & tester );
-
-// Components
-bool tCompare01( LibTest & test );
-bool tCompare02( LibTest & test );
-bool tStr01( LibTest & test ); 
-bool tStr02( LibTest & test ); 
-bool tStr03( LibTest & test ); 
-bool tStr04( LibTest & test ); 
-bool tStr05( LibTest & test ); 
-bool tStr06( LibTest & test ); 
-bool tStr07( LibTest & test ); 
-bool tStr08( LibTest & test ); 
-bool tStr09( LibTest & test ); 
-bool tStr10( LibTest & test ); 
-bool tStr11( LibTest & test ); 
-bool tStr12( LibTest & test ); 
-bool tStr13( LibTest & test ); 
-bool tStr14( LibTest & test ); 
-bool tStr15( LibTest & test ); 
-bool tStr16( LibTest & test ); 
-bool tStr17( LibTest & test ); 
-bool tStr18( LibTest & test ); 
-bool tStr19( LibTest & test ); 
-bool tStr20( LibTest & test ); 
-bool tStr21( LibTest & test ); 
-bool tStr22( LibTest & test ); 
-bool tStr23( LibTest & test ); 
-bool tStr24( LibTest & test );
-bool tStr25( LibTest & test );
-bool tStr26( LibTest & test );
-bool tSubStr01( LibTest & test );
-bool tSubStr02( LibTest & test );
-bool tSubStr03( LibTest & test );
-bool tSubStr04( LibTest & test );
-bool tSubStr05( LibTest & test );
-bool tSubStr06( LibTest & test );
-bool tSubStr07( LibTest & test );
-bool tDateTime01( LibTest & tester );
-bool tDateTime02( LibTest & tester );
-bool tDateTime03( LibTest & tester );
-bool tDateTime04( LibTest & tester );
-bool tDateTime05( LibTest & tester );
-bool tDateTime06( LibTest & tester );
-bool tDateTime07( LibTest & tester );
-bool tDateTime08( LibTest & tester );
-bool tBitmask01( LibTest & tester );
-bool tBitmask02( LibTest & tester );
-bool tBitmask03( LibTest & tester );
-bool tFilePath01( LibTest & tester );
-bool tFilePath02( LibTest & tester );
-bool tFilePath03( LibTest & tester );
-bool tFilePath04( LibTest & tester );
-bool tLogLevel( LibTest & tester );
-bool tLog01( LibTest & tester );
-bool tLog02( LibTest & tester );
-bool tLog03( LibTest & tester );
-bool tLog04( LibTest & tester );
-bool tLog05( LibTest & tester );
-
-#endif
 
 
 LibTest::TestItem   TestList[] =
 {
-#if defined( ALL )
   { "ArraySize",    	    tArraySize },
   { "DwordAlign",   	    tDwordAlign },
   { "MemOverlap",   	    tMemOverlap },
@@ -179,15 +49,15 @@ LibTest::TestItem   TestList[] =
   { "Round",	    	    tRound },
   { "Bit",    	    	    tBit },
   { "Compare", 	    	    tCompare },
-  { "SafeStrcpy",   	    tSafeStrcpy },
   { "CharToInt",    	    tCharToInt },
-  { "StripWhite",   	    tStripWhite },
-  { "StringSearch", 	    tStringSearch },
+  { "SafeStrcpy",   	    tSafeStrcpy },
+  { "StringCapitalize",	    tStringCapitalize },
   { "StringCaseCompare",    tStringCaseCompare },
   { "StringLower",  	    tStringLower },
-  { "StringUpper",  	    tStringUpper },
-  { "StringCapitalize",	    tStringCapitalize },
+  { "StringSearch", 	    tStringSearch },
   { "StringTo",  	    tStringTo },
+  { "StringUpper",  	    tStringUpper },
+  { "StripWhite",   	    tStripWhite },
   { "basename",     	    tbasename },
   { "HeapBinStream",	    tHeapBinStream },
   { "Str",  	    	    tStr },
@@ -199,20 +69,18 @@ LibTest::TestItem   TestList[] =
   { "DateRangeDaily",	    tDateRangeDaily },
   { "DateRangeWeekly",	    tDateRangeWeekly },
   { "Bitmask",		    tBitmask },
-  { "SortOrder",	    tSortOrder },
   { "FilePath",		    tFilePath },
   { "User",		    tUser },
   { "UserGroup",	    tUserGroup },
   { "FileStat",		    tFileStat },
   { "Log",		    tLog },
   { "Param",		    tParam },
+  { "SortOrder",	    tSortOrder },
+#if defined( ALL )
   { "WithCommon",	    tWithCommon },
   { "WithWcf",		    tWithWcf },
 #endif
-  { "StrSet",		    tStrSet },
-  { "Bitmask",		    tBitmask },
-  { "Log",		    tLog },
-  { "Param",		    tParam },
+  
 #ifdef COMPONENT_TEST
   { "ArraySize",    	    tArraySize },
   { "DwordAlign",   	    tDwordAlign },
