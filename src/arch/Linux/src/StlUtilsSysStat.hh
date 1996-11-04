427e6716de510fa26fc58971081f47873a25647e
houghton <>
1996-11-04 14:34:55 +0000

    Initial Version.

---
+++ src/arch/Linux/src/StlUtilsSysStat.hh
@@ -0,0 +1,29 @@
+#ifndef _ClueSysStat_hh_
+#define _ClueSysStat_hh_
+//
+// File:        ClueSysStat.hh
+// Project:	Clue
+// Desc:        
+//
+//
+//
+// Author:      Paul Houghton - (paul_houghton@wiltel.com)
+// Created:     11/01/96 07:34
+//
+// Revision History: (See end of file for Revision Log)
+//
+// $Id$
+//
+
+#include <sys/stat.h>
+
+//
+// Revision Log
+//
+// $Log$
+// Revision 2.1  1996/11/04 14:34:55  houghton
+// Initial Version.
+//
+//
+#endif // ! def _ClueSysStat_hh_ 
+
