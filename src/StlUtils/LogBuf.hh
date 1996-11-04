97971356e0a56ea5bd6d7fa333fa025499dc5b68
houghton <>
1996-11-04 14:04:07 +0000

    Restructure header comments layout. Added FilterId type for multiple destination support. Removed willOutput method (can use getLogLevel().willOutput()). Removed level() which provided direct modification access to the     LogLevel. Added setCurrentLevel method to change the current level. Added setOutputLevel method to change the output level. Added getLogLevel method to provide const access to the LogLevel. Added addFilter method to provide mutliple filtered destinations. Added delFilter method to remove a destination filter that is no     longer needed. Added static const badFilterId to detect a failed addFilter return. Changed logFileName to a RWCString instead of FilePath.     (as required by Mike Alexandar).

--- src/StlUtils/LogBuf.hh
+++ src/StlUtils/LogBuf.hh
@@ -2,43 +2,30 @@
 #define _LogBuf_hh_
 //
 // File:        LogBuf.hh
+// Project:	Clue
 // Desc:        
 //              
+//  This is a specialized streambuf used by Log to
+//  output log messages.
 //
 // Author:      Paul Houghton x2309 - (houghton@shoe.wiltel.com)
 // Created:     01/29/95 12:32
 //
-// Revision History:
+// Revision History: (See end of file for Revision Log)
 //
-// 
-// $Log$
-// Revision 2.4  1995/11/12 18:02:42  houghton
-// Change LogLevel::XXXX to LogLevel::Xxxxx.
-//
-// Revision 2.3  1995/11/10  14:11:42  houghton
-// Cleanup (move final endif)
-//
-// Revision 2.2  1995/11/10  14:08:37  houghton
-// Updated documentation comments
-//
-// Revision 2.1  1995/11/10  12:40:45  houghton
-// Change to Version 2
-//
-// Revision 1.4  1995/11/05  15:28:39  houghton
-// Revised
-//
+// $Id$
 //
 
 #if !defined( CLUE_SHORT_FN )
 #include <ClueConfig.hh>
 #include <LogLevel.hh>
-#include <FilePath.hh>
+#include <rw/cstring.h>
+#include <vector>
 #include <iostream>
 #include <fstream>
 #else
 #include <ClueCfg.hh>
 #include <LogLvl.hh>
-#include <FilePath.hh>
 #include <iostream>
 #include <fstream>
 #endif
@@ -48,10 +35,14 @@
 #define inline
 #endif
 
+class RegexScan;
+
 class LogBuf : public streambuf
 {
   
 public:
+
+  typedef long	FilterId;
   
   inline LogBuf( LogLevel::Level   outLevel,
 		 streambuf * 	   outStream );
@@ -71,15 +62,24 @@ public:
 		 ios::open_mode	    mode = ios::app,
 		 int		    prot = filebuf::openprot,
 		 size_t		    maxSize = 0,
-
 		 size_t		    trimSize = 0 );
   
   virtual ~LogBuf( void );
 
-  inline bool		willOutput( LogLevel::Level outputLevel ) const;
+  inline LogLevel::Level    setCurrentLevel( LogLevel::Level level );
+  inline LogLevel::Level    setCurrentLevel( const char * level );
 
+  inline LogLevel::Level    setOutputLevel( LogLevel::Level level );
+  inline LogLevel::Level    setOutputLevel( const char * level );
+
+  inline const LogLevel &   getLogLevel( void ) const;
+  
+  size_t		trim( size_t maxSize = 0 );
+  inline size_t	        setMaxSize( size_t maxSize );
+  inline size_t		setTrimSize( size_t trimSize );
   
   inline streambuf *	tee( streambuf * teeStreambuf );
+  
   filebuf *		open( const char *	name,
 			      ios::open_mode    mode,
 			      int	        prot = filebuf::openprot,
@@ -87,16 +87,18 @@ public:
 			      size_t	        trimSize = 0 );
   
   void			close (void);
-
-  size_t		trim( size_t maxSize = 0 );
-  inline size_t	        setMaxSize( size_t maxSize );
-  inline size_t		setTrimSize( size_t trimSize );
-  
   inline bool		isFile( void ) const;
   
-  inline LogLevel &	   level( void );
-  inline const LogLevel &  level( void ) const;
+  inline bool	filter( const char * regex );
+  
+  FilterId	addFilter( streambuf *     destBuf,
+			   LogLevel::Level outputLevel,
+			   const char *    regex = 0 );
 
+  inline bool		    delFilter( FilterId id );
+
+  
+  // streambuf virtuals
   virtual int	    overflow(int=EOF);
   virtual int	    underflow();
   virtual int	    sync() ;
@@ -108,11 +110,10 @@ public:
 				  bool		showVer = true ) const;
   
   static const ClassVersion version;
+  static const FilterId	    badFilterId;
   
 protected:
   
-private:
-
   void initLogBuffer( void );
   void initbuf( streambuf * outStream );
   void initbuf( const char * fn,
@@ -121,12 +122,25 @@ private:
 		size_t m,
 		size_t t );
   
-
+  int		sendToStream( streambuf * dest, char * base, int len );
+  
   filebuf *	openLog( void );
   size_t	trimLog( size_t curSize, size_t maxLogSize );
   void		closeLog( void );
+
+  class Filter
+  {
+  public:
+    Filter( void ) : dest(0), outputLevel( LogLevel::None ), regex( 0 ) {};
+    
+    streambuf *	    dest;
+    LogLevel::Level outputLevel;
+    RegexScan *	    regex;
+  };
+
+  typedef vector< Filter > FilterList;
   
-  FilePath	    logFileName;
+  RWCString	    logFileName;
   size_t	    maxSize;
   size_t	    trimSize;
   ios::open_mode    openMode;
@@ -136,9 +150,15 @@ private:
   char *    	buffer;
   streambuf *	stream;
   streambuf * 	teeStream;
-  
+
   LogLevel 	logLevel;
+  RegexScan *	regex;
+  bool		newMesg;
+
+  FilterList	filters;
   
+private:
+
 };
 
 #if !defined( inline )
@@ -253,6 +273,41 @@ private:
 //  Private Methods:
 //
 //  Other Associated Functions:
+//
+// Revision Log:
+//
+// $Log$
+// Revision 2.5  1996/11/04 14:04:07  houghton
+// Restructure header comments layout.
+// Added FilterId type for multiple destination support.
+// Removed willOutput method (can use getLogLevel().willOutput()).
+// Removed level() which provided direct modification access to the
+//     LogLevel.
+// Added setCurrentLevel method to change the current level.
+// Added setOutputLevel method to change the output level.
+// Added getLogLevel method to provide const access to the LogLevel.
+// Added addFilter method to provide mutliple filtered destinations.
+// Added delFilter method to remove a destination filter that is no
+//     longer needed.
+// Added static const badFilterId to detect a failed addFilter return.
+// Changed logFileName to a RWCString instead of FilePath.
+//     (as required by Mike Alexandar).
+//
+// Revision 2.4  1995/11/12 18:02:42  houghton
+// Change LogLevel::XXXX to LogLevel::Xxxxx.
+//
+// Revision 2.3  1995/11/10  14:11:42  houghton
+// Cleanup (move final endif)
+//
+// Revision 2.2  1995/11/10  14:08:37  houghton
+// Updated documentation comments
+//
+// Revision 2.1  1995/11/10  12:40:45  houghton
+// Change to Version 2
+//
+// Revision 1.4  1995/11/05  15:28:39  houghton
+// Revised
+//
 //
 
 #endif // ! def _LogBuf_hh_ 
