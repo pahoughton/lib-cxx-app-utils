ca74ade4431719c47fb3a6fe8a850542d13170a6
houghton <>
1996-11-13 17:19:19 +0000

    Added calls to new test functions (06, 07, 08 & 09).

--- test/Base/src/tLog.C
+++ test/Base/src/tLog.C
@@ -1,10 +1,23 @@
-#if !defined( CLUE_SHORT_FN )
-#include <TestConfig.hh>
-#include <LibTest.hh>
-#else
-#include <TestCfg.hh>
-#include <LibTest.hh>
-#endif
+//
+// File:        tLog.C
+// Project:	Clue
+// Desc:        
+//
+//  Test for Log class
+//
+// Source Header Version: 2.8
+//
+// Author:      Paul Houghton - (houghton@cmore.wiltel.com)
+// Created:     11/11/96 16:28
+//
+// Revision History: (See end of file for Revision Log)
+//
+// $Id$
+//
+
+
+#include "TestConfig.hh"
+#include "LibTest.hh"
 
 bool tLogLevel( LibTest & tester );
 bool tLog01( LibTest & tester );
@@ -12,6 +25,10 @@ bool tLog02( LibTest & tester );
 bool tLog03( LibTest & tester );
 bool tLog04( LibTest & tester );
 bool tLog05( LibTest & tester );
+bool tLog06( LibTest & tester );
+bool tLog07( LibTest & tester );
+bool tLog08( LibTest & tester );
+bool tLog09( LibTest & tester );
 
 
 bool
@@ -23,6 +40,17 @@ tLog( LibTest & tester )
   TESTP( tLog03( tester ) );
   TESTP( tLog04( tester ) );
   TESTP( tLog05( tester ) );
-
+  TESTP( tLog06( tester ) );
+  TESTP( tLog07( tester ) );
+  TESTP( tLog08( tester ) );
+  TESTP( tLog09( tester ) );
+  
   return( true );
 }
+
+//
+// $Log$
+// Revision 2.3  1996/11/13 17:19:19  houghton
+// Added calls to new test functions (06, 07, 08 & 09).
+//
+//
