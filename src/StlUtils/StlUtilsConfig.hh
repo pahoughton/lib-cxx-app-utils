4c3da6da23502500a71d4ea0baaa6541b3ce140d
houghton <>
1996-12-01 11:58:36 +0000

    Initial Version

---
+++ src/StlUtils/StlUtilsConfig.hh
@@ -0,0 +1,48 @@
+#ifndef _ClueConfig_hh_
+#define _ClueConfig_hh_
+//
+// File:        ClueConfig.hh
+// Project:	Clue
+// Desc:        
+//
+//  Clue Architecture independant configuration. This should
+//  be the first thing included by every component of this library.
+//  
+//  It includes the architechure dependant configuratation
+//  header (ClueArch.hh) and the Version Info for Clue classes
+//  and functions.
+//
+// Author:      Paul Houghton - (paul_houghton@wiltel.com)
+// Created:     12/01/96 04:49
+//
+// Revision History: (See end of file for Revision Log)
+//
+// $Id$
+//
+
+#include <ClueArch.hh>
+#include <ClassVersion.hh>
+#include <ClueVersion.hh>
+
+//
+// Detail Documentation
+//
+// See Also:
+//
+//  ClueArch.hh, ClassVersion.hh ClueVersion.hh
+//
+// Files:
+//
+// Documented Ver:
+//
+// Tested Ver:
+//
+// Revision Log:
+//
+// $Log$
+// Revision 3.1  1996/12/01 11:58:36  houghton
+// Initial Version
+//
+//
+#endif // ! def _ClueConfig_hh_ 
+
