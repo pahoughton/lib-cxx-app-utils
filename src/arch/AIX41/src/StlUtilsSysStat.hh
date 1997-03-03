c7171c0ec82375164cb22137b84fb2dcfca06608
houghton <>
1997-03-03 19:07:15 +0000

    Initial Version.

---
+++ src/arch/AIX41/src/StlUtilsSysStat.hh
@@ -0,0 +1,35 @@
+#ifndef _ClueSysStat_hh_
+#define _ClueSysStat_hh_
+//
+// File:        ClueSysStat.hh
+// Project:	Clue
+// Desc:        
+//
+//
+//
+// Author:      Paul Houghton - (paul_houghton@wiltel.com)
+// Created:     11/01/96 07:34
+//
+// Revision History: (See end of file for Revision Log)
+//
+// $Id$
+//
+
+#include <sys/stat.h>
+
+//
+// Revision Log
+//
+// $Log$
+// Revision 1.1  1997/03/03 19:07:15  houghton
+// Initial Version.
+//
+// Revision 3.1  1996/11/14 01:25:06  houghton
+// Changed to Release 3
+//
+// Revision 2.1  1996/11/04 14:34:55  houghton
+// Initial Version.
+//
+//
+#endif // ! def _ClueSysStat_hh_ 
+
