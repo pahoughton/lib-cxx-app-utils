999b620cb199cabd7170c0d316ade7bfb25b7d08
houghton <>
1997-03-03 19:06:38 +0000

    Initial version.

---
+++ src/arch/AIX41/src/StlUtilsArch.hh
@@ -0,0 +1,79 @@
+#ifndef _ClueArch_hh_
+#define _ClueArch_hh_
+//
+// File:        ClueArch.hh
+// Project:	Clue
+// Desc:        
+//
+//
+//
+// Quick Start: - short example of class usage
+//
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     03/03/97 09:26
+//
+// Revision History: (See end of file for Revision Log)
+//
+// $Id$
+//
+
+#define _ALL_SOURCE_
+
+/* #define CLUE_HAVE_LONG_ABS 1 */
+#define CLUE_HAS_STRPTIME 1
+#define CLUE_LOG_WHERE 1
+#define CLUE_HAS_STR 1
+#define CLUE_HAS_DATETIME 1
+/* #define CLUE_THREADS	1 */
+
+#define CLUE_STRBUF_CONST_RDBUF
+#define CLUE_STRBUF_PBASE
+
+#define CLUE_HAS_USER	1
+
+#if defined( CLUE_DEBUG )
+#define CLUE_SAFETY_ON 1
+#endif
+
+#if defined( CLUE_THREADS )
+#include <pthread.h>
+#endif
+
+// Types
+#define CLUE_CLASS_T
+#define CLUE_FUNCT_T
+#define CLUE_GVAR_T
+
+#define CLUE_CHAR   char
+#define CLUE_SCHAR  signed char
+#define CLUE_UCHAR  unsigned char
+#define CLUE_16	    short
+#define CLUE_U16    unsigned short
+#define CLUE_INT    int
+#define CLUE_UINT   unsigned int
+#define CLUE_32	    long
+#define CLUE_U32    unsigned long
+#define CLUE_FLOAT  float
+#define CLUE_DOUBLE double
+#define CLUE_BOOL   bool
+
+// Constants
+#define CLUE_DIR_DELIM	    '/'
+#define CLUE_BAD_FPOS_T	    LONG_MIN
+
+#define NPOS ((size_t)-1)
+// Macros
+#define CLUE_UNUSED( x ) x
+
+#include <AnsiBool.hh>
+
+
+// Revision Log:
+//
+// $Log$
+// Revision 1.1  1997/03/03 19:06:38  houghton
+// Initial version.
+//
+//
+#endif // ! def _ClueArch_hh_ 
+
