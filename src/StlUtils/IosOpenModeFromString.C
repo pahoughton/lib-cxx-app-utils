1b49c3766dd8146a827ef92eaefa64300c3fbfcb
houghton <>
1997-04-04 20:51:48 +0000

    Initial Versionn.

---
+++ src/StlUtils/IosOpenModeFromString.C
@@ -0,0 +1,65 @@
+//
+// File:        IosOpenModeFromString.C
+// Project:	Clue
+// Desc:        
+//
+//  Compiled sources for IosOpenModeFromString
+//  
+// Author:      Paul A. Houghton - (paul.houghton@wcom.com)
+// Created:     04/04/97 08:24
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+
+#include "StringUtils.hh"
+#include <iostream>
+
+
+CLUE_FUNCT_VERSION(
+  IosOpenModeFromString,
+  "$Id$" );
+
+
+ios::open_mode
+IosOpenModeFromString( const char * modeString )
+{
+  ios::open_mode    mode = (ios::open_mode)0;
+
+  if( StringCaseSearch( modeString, NPOS, "in", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::in);
+
+  if( StringCaseSearch( modeString, NPOS, "out", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::out);
+
+  if( StringCaseSearch( modeString, NPOS, "ate", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::ate);
+
+  if( StringCaseSearch( modeString, NPOS, "app", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::app);
+
+  if( StringCaseSearch( modeString, NPOS, "trunc", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::trunc);
+
+  if( StringCaseSearch( modeString, NPOS, "nocreate", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::nocreate);
+  
+  if( StringCaseSearch( modeString, NPOS, "noreplace", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::noreplace);
+  
+  if( StringCaseSearch( modeString, NPOS, "binary", NPOS ) )
+    mode = (ios::open_mode)( mode | ios::binary);
+  
+  return( mode );
+}
+
+// Revision Log:
+//
+// $Log$
+// Revision 3.1  1997/04/04 20:51:45  houghton
+// Initial Versionn.
+//
+//
---
+++ src/StlUtils/IosOpenModeToString.C
@@ -0,0 +1,69 @@
+//
+// File:        IosOpenModeToString.C
+// Project:	Clue
+// Desc:        
+//
+//  Compiled sources for IosOpenModeToString
+//  
+// Author:      Paul A. Houghton - (paul.houghton@wcom.com)
+// Created:     04/04/97 08:59
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+
+#include "Str.hh"
+
+
+CLUE_FUNCT_VERSION(
+  IosOpenModeToString,
+  "$Id$" );
+
+const char *
+IosOpenModeToString( ios::open_mode mode )
+{
+  static Str modeStr;
+
+  modeStr = "";
+  
+  if( mode & ios::in )
+    modeStr << "ios::in | ";
+
+  if( mode & ios::out )
+    modeStr << "ios::out | ";
+
+  if( mode & ios::ate )
+    modeStr << "ios::ate | ";
+
+  if( mode & ios::app )
+    modeStr << "ios::app | ";
+
+  if( mode & ios::trunc )
+    modeStr << "ios::trunc | ";
+
+  if( mode & ios::nocreate )
+    modeStr << "ios::nocreate | ";
+
+  if( mode & ios::noreplace )
+    modeStr << "ios::noreplace | ";
+
+  if( mode & ios::binary )
+    modeStr << "ios::binary | ";
+
+  if( modeStr.size() > 3 )
+    modeStr.remove( modeStr.size() - 3 );
+
+  return( modeStr.c_str() );
+}
+
+  
+// Revision Log:
+//
+// $Log$
+// Revision 3.1  1997/04/04 20:51:48  houghton
+// Initial Versionn.
+//
+//
