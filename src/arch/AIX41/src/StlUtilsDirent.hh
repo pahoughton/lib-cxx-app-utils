a6808721d138609aeebab7055507082317880e91
houghton <>
1997-06-09 14:29:02 +0000

    Initial Version.

---
+++ src/arch/AIX41/src/StlUtilsDirent.hh
@@ -0,0 +1,35 @@
+#ifndef _ClueDirent_hh_
+#define _ClueDirent_hh_
+//
+// File:        ClueDirent.hh
+// Project:	Clue
+// Desc:        
+//
+//
+//
+// Quick Start: - short example of class usage
+//
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     06/09/97 07:12
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+//  $Id$
+//
+
+#include <dirent.h>
+
+//
+// Revision Log:
+//
+// $Log$
+// Revision 1.1  1997/06/09 14:29:02  houghton
+// Initial Version.
+//
+//
+#endif // ! def _ClueDirent_hh_ 
+
