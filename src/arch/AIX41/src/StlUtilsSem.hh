bdb5fa5a982e2052df0da2a0634a3be474c5c381
houghton <>
1997-07-14 10:31:12 +0000

    Initial version.

---
+++ src/arch/AIX41/src/StlUtilsSem.hh
@@ -0,0 +1,41 @@
+#ifndef _ClueSem_hh_
+#define _ClueSem_hh_
+//
+// File:        ClueSem.hh
+// Project:	Clue
+// Desc:        
+//
+//
+//
+// Quick Start: - short example of class usage
+//
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     07/11/97 11:04
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+//  $Id$
+//
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <errno.h>
+
+#define CLUE_SEM_BADKEY	    -1
+#define CLUE_SEM_BADSEM	    -1
+
+//
+// Revision Log:
+//
+// $Log$
+// Revision 1.1  1997/07/14 10:31:12  houghton
+// Initial version.
+//
+//
+#endif // ! def _ClueSem_hh_ 
+
