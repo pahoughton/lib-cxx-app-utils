e46e4c34a8a5ff34a54e22ca5e81e430089bfc26
houghton <>
1997-08-28 16:37:50 +0000

    Changed to use two operator output files (01 & 02). Added test to verify setw(n) works correctly.

--- test/Base/src/tSubStr05.C
+++ test/Base/src/tSubStr05.C
@@ -5,6 +5,7 @@
 #include <strstream.h>
 #include <fstream>
 #include <cstring>
+#include <iomanip>
 #else
 #include <TestConfig.hh>
 #include <LibTest.hh>
@@ -52,9 +53,9 @@ tSubStr05( LibTest & tester )
     // toStream( ostream & ) const
 
 #if !defined( CLUE_SHORT_FN )
-    const char * fn = TEST_DATA_DIR "/SubStr.operator.output";
+    const char * fn = TEST_DATA_DIR "/SubStr.operator.output.01";
 #else
-    const char * fn = TEST_DATA_DIR "/ssopout.txt";
+    const char * fn = TEST_DATA_DIR "/ssopot1.txt";
 #endif
     ofstream out( fn );
     
@@ -70,15 +71,24 @@ tSubStr05( LibTest & tester )
     // operator << ( ostream &, const Str & )
 
 #if !defined( CLUE_SHORT_FN )
-    const char * fn = TEST_DATA_DIR "/SubStr.operator.output";
+    const char * fn = TEST_DATA_DIR "/SubStr.operator.output.02";
 #else
-    const char * fn = TEST_DATA_DIR "/ssopout.txt";
+    const char * fn = TEST_DATA_DIR "/ssopot2.txt";
 #endif
     ofstream out( fn );
     
     Str t( "123" OutStr "junk" );
     
     out << t.substr( 3, strlen( OutStr ) );
+
+    t = "substr width test";
+    out.fill( '#' );
+    out << '\'' <<  setw( 20 ) << t.substr( 0, 6 ) << '\'' <<  endl;
+
+    out.setf( ios::left, ios::adjustfield );
+    out << '\'' <<  setw(20) << t.substr( 0, 6 ) << '\'' <<  endl;
+
+    out << '\'' <<  t.substr( 0, 6 ) << '\'' << endl;
     out.close();
     tester.file( __FILE__, __LINE__, fn );
   }
