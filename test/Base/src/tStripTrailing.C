3444dfd6b8ecb3dfd3d28fbdec25c81097b41e4b
houghton <>
1997-12-20 16:13:47 +0000

    Initial Version.

---
+++ test/Base/src/tStripTrailing.C
@@ -0,0 +1,59 @@
+//
+// File:        tStripTrailing.C
+// Project:	StlUtils
+// Desc:        
+//
+//  Compiled sources for tStripTrailing
+//  
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     12/20/97 08:20
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+
+#include <TestConfig.hh>
+#include <LibTest.hh>
+#include <StringUtils.hh>
+
+bool
+tStripTrailing( LibTest & tester )
+{
+  {
+    char t[ 1024 ];
+
+    const char * exp = "this is a test";
+
+    strcpy( t, exp );
+
+    StripTrailing( t );
+    TEST( strcmp( t, exp ) == 0 );
+
+    strcpy( t, exp );
+    strcat( t, "\t\t  " );
+
+    StripTrailing( t );
+    TEST( strcmp( t, exp ) == 0 );
+
+    strcpy( t, "\t\t  \t\t" );
+
+    StripTrailing( t );
+    TEST( t[0] == 0 );
+  }
+
+  return( true );
+}
+
+
+    
+
+// Revision Log:
+//
+// $Log$
+// Revision 1.1  1997/12/20 16:13:47  houghton
+// Initial Version.
+//
+//
