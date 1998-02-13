9c3d8c091fa12858b0e19f08fe2211a3e4822911
houghton <>
1998-02-13 01:21:31 +0000

    Initial version.

---
+++ src/arch/Linux/src/_StlUtilsSignal.h
@@ -0,0 +1,52 @@
+#ifndef __StlUtilsSignal_h_
+#define __StlUtilsSignal_h_
+/*********************************************************************
+ *
+ * Title:       _StlUtilsSignal.h
+ * Project:	StlUtils
+ * Desc:
+ *
+ *  
+ *
+ * Notes:
+ *
+ * Author:	Paul A. Houghton - (paul.houghton@wcom.com)
+ * Created:	02/03/98 06:25
+ *
+ * Revision History: (See end of file for Revision Log)
+ *
+ *  Last Mod By:    $Author$
+ *  Last Mod:	    $Date$
+ *  Version:	    $Revision$
+ *
+ * $Id$
+ *
+ *********************************************************************/
+
+#include <signal.h>
+
+#define SA_ONSTACK	0
+#define SA_SIGINFO	0
+#define SA_NOCLDWAIT	0
+#define SA_WAITSIG	0
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+#if defined( __cplusplus )
+};
+#endif
+
+/*
+ *
+ * Revision Log:
+ *
+ * $Log$
+ * Revision 4.1  1998/02/13 01:21:31  houghton
+ * Initial version.
+ *
+ *
+ */
+
+#endif /* ! def __StlUtilsSignal_h_ */
