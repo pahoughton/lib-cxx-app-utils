9c57b77437a0c3e5ec1e848730389dd59ef9902e
houghton <>
1998-02-13 22:13:29 +0000

    Initial version

---
+++ src/arch/Sun5/src/_StlUtilsSignal.h
@@ -0,0 +1,47 @@
+#ifndef __StlUtilsSignal_h_
+#define __StlUtilsSignal_h_
+/*********************************************************************
+ *
+ * Title:       _StlUtilsSignal.h
+ * Project:	StlUtils
+ * Desc:
+ *
+ *  
+ *
+ * Notes:
+ *
+ * Author:	Paul Houghton - (paul.houghton@wcom.com)
+ * Created:	02/12/98 19:24
+ *
+ * Revision History: (See end of file for Revision Log)
+ *
+ *  Last Mod By:    $Author$
+ *  Last Mod:	    $Date$
+ *  Version:	    $Revision$
+ *
+ * $Id$
+ *
+ *********************************************************************/
+
+#include <signal.h>
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+#if defined( __cplusplus )
+};
+#endif
+
+/*
+ *
+ * Revision Log:
+ *
+ * $Log$
+ * Revision 1.1  1998/02/13 22:13:29  houghton
+ * Initial version
+ *
+ *
+ */
+
+#endif /* ! def __StlUtilsSignal_h_ */
