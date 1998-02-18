f1ebae8fc6c7c5c96b98adf74e49888ea0019c1b
houghton <>
1998-02-18 14:48:32 +0000

    Initial Version.

---
+++ src/arch/AIX41/src/_StlUtilsSignal.h
@@ -0,0 +1,53 @@
+#ifndef __StlUtilsSignal_h_
+#define __StlUtilsSignal_h_
+/*********************************************************************
+ *
+ * Title:       _StlUtilsSignal.h
+ * Project:	StlUtils
+ * Desc:
+ *
+ *  
+ *
+ * Notes:
+ *
+ * Author:	Paul Houghton - (paul.houghton@wcom.com)
+ * Created:	02/18/98 08:29
+ *
+ * Revision History: (See end of file for Revision Log)
+ *
+ *  Last Mod By:    $Author$
+ *  Last Mod:	    $Date$
+ *  Version:	    $Revision$
+ *
+ * $Id$
+ *
+ *********************************************************************/
+
+#include <signal.h>
+
+#define SA_NODEFER	0
+#define SA_SIGINFO	0
+#define SA_NOCLDWAIT	0
+#define SA_WAITSIG	0
+
+#if defined( __cplusplus )
+extern "C" {
+#endif
+
+
+#if defined( __cplusplus )
+};
+#endif
+
+/*
+ *
+ * Revision Log:
+ *
+ * $Log$
+ * Revision 1.1  1998/02/18 14:48:32  houghton
+ * Initial Version.
+ *
+ *
+ */
+
+#endif /* ! def __StlUtilsSignal_h_ */
