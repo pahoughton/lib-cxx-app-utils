0c01db1d6d641a243f97aa350a17b2bf1e5eccfa
houghton <>
1999-05-01 12:56:42 +0000

    Initial Version.

---
+++ test/Base/src/tStr27.C
@@ -0,0 +1,48 @@
+//
+// File:        tStr27.C
+// Project:	StlUtils
+// Desc:        
+//
+//  Compiled sources for tStr27
+//  
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     03/02/99 05:32
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+
+#include <Str.hh>
+
+class George
+{
+public:
+  George() {};
+
+private:
+  Str::RangeList t;
+};
+
+
+bool
+tStr27( void )
+{
+  {
+    George x;
+    // Str::RangeList  t;
+
+  }
+
+  return( true );
+}
+
+// Revision Log:
+//
+// $Log$
+// Revision 4.1  1999/05/01 12:56:42  houghton
+// Initial Version.
+//
+//
