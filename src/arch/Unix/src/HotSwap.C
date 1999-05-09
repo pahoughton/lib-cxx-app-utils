d4810d91320de70f41901cc0c87241b2bd37471e
houghton <>
1999-05-09 13:08:19 +0000

    Initial Version.

---
+++ src/arch/Unix/src/HotSwap.C
@@ -0,0 +1,338 @@
+//
+// File:        HotSwap.C
+// Project:	StlUtils
+// Desc:        
+//
+//  Compiled sources for HotSwap
+//  
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     05/04/99 05:32
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+
+#include "HotSwap.hh"
+
+#include <FileStat.hh>
+
+#include <cstdio>
+
+#include <unistd.h>
+
+#if defined( STLUTILS_DEBUG )
+#include "HotSwap.ii"
+#endif
+
+STLUTILS_VERSION(
+  HotSwap,
+  "$Id$");
+
+const char * HotSwap::ErrorDesc[] =
+{
+  "ok",
+  "not a symbolic link",
+  0
+};
+
+
+HotSwap::HotSwap(
+  const FilePath & lockFileName,
+  ios::open_mode    lockMode
+  )
+  : lockFn( lockFileName ),
+    filelock( lockFn, lockMode ),
+    errorNum( E_OK )
+{
+  if( ! sem.open( lockFn ) )
+    {
+      // could not open sem, try to create
+      if( ! sem.create( lockFn,
+			' ',
+			1,
+			(IPC_CREAT | IPC_EXCL | 0664) ) )
+	{
+	  // could not create, some one else probably already did
+	  // wait a second and try again.
+	  sleep( 1 );
+	  if( ! sem.open( lockFn ) )
+	    return;
+	}
+    }
+}
+
+HotSwap::~HotSwap( void )
+{
+}
+
+bool
+HotSwap::lock( void )
+{
+  
+  if( sem.islocked() )
+    {      
+      if( ! sem.waitfor() )
+	return( false );
+
+      if( ! filelock.lockread() )
+	return( false );
+    }
+
+  return( true );
+}
+
+bool
+HotSwap::waitForSwap( void )
+{
+  if( ! filelock.good() )    return( false );
+  if( ! filelock.unlock() )   return( false );
+  if( ! sem.waitfor() )      return( false );
+  if( ! filelock.lockread() ) return( false );
+
+  return( true );
+}
+
+bool
+HotSwap::check( void )
+{
+  return( sem.islocked() );
+}
+
+bool
+HotSwap::swap(
+  const FilePath &  linkName,
+  const FilePath &  linkDest
+  )
+{
+  // use absolute file names.
+  FilePath  absLinkDest;
+
+  if( linkDest[0] != '/' )
+    {
+      char cwd[1024];
+      
+      if( ! getcwd( cwd, sizeof( cwd ) ) )
+	return( setErrorDesc( errno, "getcwd" ) );
+      
+      absLinkDest << cwd << linkDest.dirSep() << linkDest ;
+    }
+  else
+    {
+      absLinkDest = linkDest;
+    }
+  
+  FileStat  linkStat( linkName, true );
+
+  bool	    linkExists;
+  
+  if( linkStat.good() )
+    {
+      linkExists = true;
+      
+      if( ! linkStat.isLink() )
+	return( setErrorFn( E_NOT_LINK, linkName ) );
+    }
+  else
+    {
+      linkExists = false;
+    }
+
+  // only do locking if a link already exists
+
+  if( linkExists )
+    {
+      if( ! sem.lock( false ) )
+	  return( false );
+      
+      if( ! filelock.lockwrite() )
+	{
+	  sem.unlock();
+	  return( false );
+	}
+
+      // ok, all locked up.
+      
+      if( remove( linkName ) )
+	{
+	  setErrorFn( errno, linkName );
+	  filelock.unlock();
+	  sem.unlock();
+	  return( false );
+	}
+    }
+
+  if( symlink( absLinkDest, linkName ) )
+    {
+      setErrorFnDesc( errno, linkName, "creating link" );
+      filelock.unlock();
+      sem.unlock();
+      return( false );
+    }
+
+  if( linkExists )
+    {
+      if( sem.good() )
+	{
+	  if( ! filelock.unlock() )
+	    {
+	      sem.unlock();
+	      return( false );
+	    }
+	  
+	  if( ! sem.unlock() )
+	    return( false );
+	}
+    }
+  
+  return( true );
+}
+
+bool
+HotSwap::good( void ) const
+{
+  return( filelock.good()
+	  && sem.good()
+	  && osErrno == 0
+	  && errorNum == E_OK );
+}
+
+const char *
+HotSwap::error( void ) const
+{
+  static Str errStr;
+
+  errStr = HotSwap::getClassName();
+
+  if( good() )
+    {
+      errStr << ": ok";
+    }
+  else
+    {
+      size_t eSize = errStr.size();
+
+      if( ! filelock.good() )
+	errStr << ": " << filelock.error();
+
+      if( ! sem.good() )
+	errStr << ": " << sem.error();
+
+      if( errorFileName.size() )
+	{
+	  errStr << ": '" << errorFileName << "' -";
+	  if( errorDesc.size() )
+	    errStr << errorDesc;
+	}
+      else
+	{
+	  if( errorDesc.size() )
+	    errStr << ": " << errorDesc;
+	  else
+	    errStr << ": ";
+	}
+	      
+      if( errorNum != E_OK )
+	errStr << " " << ErrorDesc[ errorNum ];
+      
+      if( osErrno )
+	errStr << " " << strerror( osErrno );
+      
+      if( eSize == errStr.size() )
+        errStr << ": unknown error";
+    }
+
+  return( errStr.c_str() );
+}
+
+const char *
+HotSwap::getClassName( void ) const
+{
+  return( "HotSwap" );
+}
+
+const char *
+HotSwap::getVersion( bool withPrjVer ) const
+{
+  return( version.getVer( withPrjVer ) );
+}
+
+
+ostream &
+HotSwap::dumpInfo(
+  ostream &	dest,
+  const char *	prefix,
+  bool		showVer
+  ) const
+{
+  if( showVer )
+    dest << HotSwap::getClassName() << ":\n"
+	 << HotSwap::getVersion() << '\n';
+
+  if( ! HotSwap::good() )
+    dest << prefix << "Error: " << HotSwap::error() << '\n';
+  else
+    dest << prefix << "Good" << '\n';
+
+
+  dest << prefix << "lockFn:  '" << lockFn << "'\n";
+  
+  Str pre;
+  
+  pre = prefix;
+  pre << "filelock: ";
+  filelock.dumpInfo( dest, pre, false );
+
+  pre = prefix;
+  pre << "sem:";
+  sem.dumpInfo( dest, pre, false );  
+  
+  return( dest );
+}
+
+bool
+HotSwap::setErrorDesc( int osErr, const char * desc )
+{
+  osErrno = osErr;
+  errorDesc = desc;
+  return( good() );
+}
+
+bool
+HotSwap::setErrorFn( ErrorNum err, const char * fn )
+{
+  errorNum	= err;
+  errorFileName	= fn;
+  return( good() );
+}
+
+bool
+HotSwap::setErrorFn( int osErr, const char * fn )
+{
+  osErrno	= osErr;
+  errorFileName	= fn;
+
+  return( good() );
+}
+
+bool
+HotSwap::setErrorFnDesc( int osErr, const char * fn, const char * desc )
+{
+  osErrno	= osErr;
+  errorFileName	= fn;
+  errorDesc	= desc;
+
+  return( good() );
+}
+
+
+  
+// Revision Log:
+//
+// $Log$
+// Revision 4.1  1999/05/09 13:08:19  houghton
+// Initial Version.
+//
+//
---
+++ src/arch/Unix/src/HotSwap.hh
@@ -0,0 +1,207 @@
+#ifndef _HotSwap_hh_
+#define _HotSwap_hh_
+//
+// File:        HotSwap.hh
+// Project:	StlUtils
+// Desc:        
+//
+//
+//
+// Quick Start: - short example of class usage
+//
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     05/04/99 05:21
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+//  $Id$
+//
+
+#include <StlUtilsConfig.hh>
+
+#include <FilePath.hh>
+#include <FileLock.hh>
+#include <Semaphore.hh>
+
+#include <DumpInfo.hh>
+#include <iostream>
+
+#if defined( STLUTILS_DEBUG )
+#define inline
+#endif
+
+
+class HotSwap
+{
+
+public:
+
+  HotSwap( const FilePath &	lockFileName,
+	   ios::open_mode	mode );
+
+  virtual ~HotSwap( void );
+
+  // user methods
+  bool	    lock( void );
+  bool	    check( void );
+  bool	    waitForSwap( void );
+
+  // swapper method
+  bool	    swap( const FilePath &  linkName,
+		  const FilePath &  linkDest );
+  
+  virtual bool	    	good( void ) const;
+  virtual const char * 	error( void ) const;
+  virtual const char *	getClassName( void ) const;
+  virtual const char *  getVersion( bool withPrjVer = true ) const;
+  virtual ostream &     dumpInfo( ostream &	dest = cerr,
+				  const char *  prefix = "    ",
+                                  bool          showVer = true ) const;
+
+  inline DumpInfo< HotSwap >
+  dump( const char * preifx = "    ", bool showVer = true ) const;
+
+  static const ClassVersion version;
+
+protected:
+
+  enum ErrorNum
+  {
+    E_OK,
+    E_NOT_LINK,
+    E_UNDEFINED
+  };
+
+  bool	setError( int osErr, const char * desc );
+  bool	setErrorDesc( int osErr, const char * desc );
+  bool	setErrorFn( ErrorNum err, const char * fn );
+  bool	setErrorFn( int osErr, const char * fn );
+  bool	setErrorFnDesc( int osErr, const char * fn, const char * desc );
+  
+  static const char *	ErrorDesc[];
+  
+  FilePath	lockFn;
+  Semaphore	sem;
+  FileLock 	filelock;
+
+  ErrorNum	errorNum;
+  FilePath	errorFileName;
+  Str		errorDesc;
+  int		osErrno;
+  
+private:
+
+  HotSwap( const HotSwap & from );
+  HotSwap & operator =( const HotSwap & from );
+
+};
+
+#if !defined( inline )
+#include <HotSwap.ii>
+#else
+#undef inline
+
+ostream &
+operator << ( ostream & dest, const HotSwap & src );
+
+istream &
+operator >> ( istream & src, const HotSwap & dest );
+
+
+#endif
+
+
+//
+// Detail Documentation
+//
+//  Data Types: - data types defined by this header
+//
+//  	HotSwap	class
+//
+//  Constructors:
+//
+//  	HotSwap( );
+//
+//  Destructors:
+//
+//  Public Interface:
+//
+//	virtual ostream &
+//	write( ostream & dest ) const;
+//	    write the data for this class in binary form to the ostream.
+//
+//	virtual istream &
+//	read( istream & src );
+//	    read the data in binary form from the istream. It is
+//	    assumed it stream is correctly posistioned and the data
+//	    was written to the istream with 'write( ostream & )'
+//
+//	virtual ostream &
+//	toStream( ostream & dest ) const;
+//	    output class as a string to dest (used by operator <<)
+//
+//	virtual istream &
+//	fromStream( istream & src );
+//	    Set this class be reading a string representation from
+//	    src. Returns src.
+//
+//  	virtual Bool
+//  	good( void ) const;
+//  	    Return true if there are no detected errors associated
+//  	    with this class, otherwise false.
+//
+//  	virtual const char *
+//  	error( void ) const;
+//  	    Return a string description of the state of the class.
+//
+//  	virtual const char *
+//  	getClassName( void ) const;
+//  	    Return the name of this class (i.e. HotSwap )
+//
+//  	virtual const char *
+//  	getVersion( bool withPrjVer = true ) const;
+//  	    Return the version string of this class.
+//
+//	virtual ostream &
+//	dumpInfo( ostream & dest, const char * prefix, bool showVer );
+//	    output detail info to dest. Includes instance variable
+//	    values, state info & version info.
+//
+//	static const ClassVersion version
+//	    Class and project version information. (see ClassVersion.hh)
+//
+//  Protected Interface:
+//
+//  Private Methods:
+//
+//  Associated Functions:
+//
+//  	ostream &
+//  	operator <<( ostream & dest, const HotSwap & src );
+//
+//	istream &
+//	operator >> ( istream & src, HotSwap & dest );
+//
+// Example:
+//
+// See Also:
+//
+// Files:
+//
+// Documented Ver:
+//
+// Tested Ver:
+//
+// Revision Log:
+//
+// $Log$
+// Revision 4.1  1999/05/09 13:08:19  houghton
+// Initial Version.
+//
+//
+#endif // ! def _HotSwap_hh_ 
+
