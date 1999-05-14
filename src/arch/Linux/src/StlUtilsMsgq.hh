85b431d40d5bdabbe78bc2ebbc2d8622b5bf88d9
houghton <>
1999-05-14 11:35:10 +0000

    Initial Version.

---
+++ src/arch/Linux/src/StlUtilsMsgq.hh
@@ -0,0 +1,51 @@
+#ifndef _StlUtilsMsgq_hh_
+#define _StlUtilsMsgq_hh_
+//
+// File:        StlUtilsMsgq.hh
+// Project:	StlUtils
+// Desc:        
+//
+//
+//
+// Quick Start: - short example of class usage
+//
+// Author:      Paul A. Houghton - (paul.houghton@wcom.com)
+// Created:     05/01/99 12:43
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+//  $Id$
+//
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
+
+#define STLUTILS_MSGQ_BADKEY	    -1
+#define STLUTILS_MSGQ_BADQ	    -1
+
+//
+// Example:
+//
+// See Also:
+//
+// Files:
+//
+// Documented Ver:
+//
+// Tested Ver:
+//
+// Revision Log:
+//
+// $Log$
+// Revision 1.1  1999/05/14 11:35:10  houghton
+// Initial Version.
+//
+//
+#endif // ! def _StlUtilsMsgq_hh_ 
+
